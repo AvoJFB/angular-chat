3945c135575a6dff903402b737cd924c6914f43e
avojfb <avetik.harutyunyan1@gmail.com>
2017-06-15 11:23:03 +0300

    Fix enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,4 +53,12 @@ export class AppComponent implements OnInit {
     });
     this.msg = '';
   }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+      e.preventDefault();
+      this.handleSubmitNewMessage();
+    }
+  }
+
 }
